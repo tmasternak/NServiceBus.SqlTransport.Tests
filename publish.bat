0e2f2097c5afa1d062f22581b29f7e641dd8b183
Tomek Masternak <tomasz.masternak@particular.net>
2020-04-28 11:47:49 +0200

    publish fixes

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
-rmdir /s /q ./output
+rmdir /s /q output 
 
 dotnet publish ./src/NServiceBus.SqlTransport.Tests.Receiver  -o ./output/receiver --self-contained true --runtime win-x64
 dotnet publish ./src/NServiceBus.SqlTransport.Tests.Sender  -o ./output/sender --self-contained true --runtime win-x64
