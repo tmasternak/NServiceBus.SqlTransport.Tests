53def58a84b0d014b00a1de332a04c3cf7fd7954
Tomek Masternak <tomasz.masternak@particular.net>
2020-05-08 12:05:07 +0200

    sender moved to system.commandline

--- src/NServiceBus.SqlTransport.Tests.Sender/Program.cs
+++ src/NServiceBus.SqlTransport.Tests.Sender/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.DragonFruit;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using NServiceBus.SqlTransport.Tests.Shared;
@@ -9,7 +12,10 @@ namespace NServiceBus.SqlTransport.Tests.Sender
 {
     public class Program
     {
-        static async Task Main(string[] a)
+        static CancellationToken ct;
+        static IEndpointInstance endpoint;
+
+        static async Task Main(string[] args)
         {
             var configuration = new EndpointConfiguration(Shared.Configuration.SenderEndpointName);
 
@@ -23,138 +29,159 @@ namespace NServiceBus.SqlTransport.Tests.Sender
 
             configuration.EnableInstallers();
 
-            var endpoint = await Endpoint.Start(configuration);
+            endpoint = await Endpoint.Start(configuration);
 
-            var commands = new (string, Func<CancellationToken, string[], Task>)[]
+            var rootCommand = new RootCommand();
+
+            rootCommand.AddCommand(CreateChildCommand(nameof(Send), "send"));
+            rootCommand.AddCommand(CreateChildCommand(nameof(ThrottledSend), "throttled-send"));
+            rootCommand.AddCommand(CreateChildCommand(nameof(FillQueue), "fill"));
+            rootCommand.AddCommand(CreateChildCommand(nameof(ResetStatistics), "reset"));
+
+            var ctSource = new CancellationTokenSource();
+            ct = ctSource.Token;
+
+            var task = rootCommand.InvokeAsync(args);
+
+            while (ctSource.IsCancellationRequested == false && task.IsCompleted == false)
             {
-                ("f|Fill the sender queue. Syntax: f <number of messages> <number of tasks> <destination>",
-                    async (ct, args) =>
-                    {
-                        var totalMessages = args.Length > 0 ? int.Parse(args[0]) : 1000;
-                        var numberOfTasks = args.Length > 1 ? int.Parse(args[1]) : 5;
-
-                        var tasks = Enumerable.Range(1, numberOfTasks).Select(async _ =>
-                        {
-                            for (var i = 0; i < totalMessages / numberOfTasks; i++)
-                            {
-                                var op = new SendOptions();
-                                if (args.Length > 2)
-                                {
-                                    op.SetDestination(args[2]);
-                                }
-
-                                await endpoint.Send(new TestCommand(), op);
-                            }
-                        }).ToArray();
-
-                        await Task.WhenAll(tasks);
-                    }),
-                ("s|Start sending messages to the queue. Syntax: s <number of tasks> <destination>", async (ct, args) =>
+                if (Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Enter)
                 {
-                    var numberOfTasks = args.Length > 0 ? int.Parse(args[0]) : 5;
+                    ctSource.Cancel();
+                    break;
+                }
 
-                    var tasks = Enumerable.Range(1, numberOfTasks).Select(async _ =>
+                await Task.Delay(TimeSpan.FromMilliseconds(500));
+            }
+
+            await task;
+        }
+
+        static Command CreateChildCommand(string methodName, string name)
+        {
+            var childCommand = new Command(name);
+            childCommand.ConfigureFromMethod(typeof(Program).GetMethod(nameof(methodName),
+                BindingFlags.Static | BindingFlags.NonPublic));
+            return childCommand;
+        }
+
+        static async Task ResetStatistics()
+        {
+            await endpoint.Send(new ResetCommand());
+        }
+
+        static async Task ThrottledSend(int numberOfMessages, string destination, int maxSenderCount = 20)
+        {
+            var taskBarriers = new int[maxSenderCount];
+
+            var monitor = Task.Run(async () =>
+            {
+                var nextTask = 0;
+
+                while (ct.IsCancellationRequested == false)
+                {
+                    try
                     {
-                        while (ct.IsCancellationRequested == false)
+                        var queueLength = await QueueLengthMonitor.GetQueueLengthMetric(destination);
+                        var delta = numberOfMessages - queueLength;
+
+                        if (delta > 0)
+                        {
+                            Interlocked.Exchange(ref taskBarriers[nextTask], 1);
+
+                            nextTask = Math.Min(maxSenderCount - 1, nextTask + 1);
+                        }
+                        else
+                        {
+                            nextTask = Math.Max(0, nextTask - 1);
+
+                            Interlocked.Exchange(ref taskBarriers[nextTask], 0);
+                        }
+
+                        await Task.Delay(TimeSpan.FromSeconds(2), ct);
+                    }
+                    catch
+                    {
+                        if (ct.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                    }
+                }
+            }, ct);
+
+
+            var senders = Enumerable.Range(0, maxSenderCount).Select(async taskNo =>
+            {
+                while (ct.IsCancellationRequested == false)
+                {
+                    try
+                    {
+                        var allowed = Interlocked.CompareExchange(ref taskBarriers[taskNo], 1, 1);
+
+                        if (allowed == 1)
                         {
                             var op = new SendOptions();
-                            if (args.Length > 2)
-                            {
-                                op.SetDestination(args[2]);
-                            }
+                            op.SetDestination(destination);
 
                             await endpoint.Send(new TestCommand(), op);
                         }
-                    }).ToArray();
-
-                    await Task.WhenAll(tasks);
-                }),
-                ("t|Throttled sending that keeps the receiver queue size at n. Syntax: t <number of msgs> <destination>",
-                    async (ct, args) =>
+                        else
+                        {
+                            await Task.Delay(TimeSpan.FromSeconds(1), ct);
+                        }
+                    }
+                    catch
                     {
-                        var maxSenderCount = 20;
-                        var taskBarriers = new int[maxSenderCount];
-
-                        var numberOfMessages = int.Parse(args[0]);
-                        var destination = args[1];
-
-                        var semaphore = new SemaphoreSlim(0, maxSenderCount);
-
-                        var monitor = Task.Run(async () =>
+                        if (ct.IsCancellationRequested)
                         {
-                            var nextTask = 0;
+                            return;
+                        }
+                    }
+                }
+            }).ToArray();
 
-                            while (ct.IsCancellationRequested == false)
-                            {
-                                try
-                                {
-                                    var queueLength = await QueueLengthMonitor.GetQueueLengthMetric(destination);
-                                    var delta = numberOfMessages - queueLength;
-
-                                    if (delta > 0)
-                                    {
-                                        Interlocked.Exchange(ref taskBarriers[nextTask], 1);
-
-                                        nextTask = Math.Min(maxSenderCount - 1, nextTask + 1);
-                                    }
-                                    else
-                                    {
-                                        nextTask = Math.Max(0, nextTask - 1);
-
-                                        Interlocked.Exchange(ref taskBarriers[nextTask], 0);
-
-                                    }
-
-                                    await Task.Delay(TimeSpan.FromSeconds(2), ct);
-                                }
-                                catch
-                                {
-                                    if (ct.IsCancellationRequested)
-                                    {
-                                        return;
-                                    }
-                                }
-                            }
-                        }, ct);
-
-
-                        var senders = Enumerable.Range(0, maxSenderCount).Select(async taskNo =>
-                        {
-                            while (ct.IsCancellationRequested == false)
-                            {
-                                try
-                                {
-                                    var allowed = Interlocked.CompareExchange(ref taskBarriers[taskNo], 1, 1);
-
-                                    if (allowed == 1)
-                                    {
-                                        var op = new SendOptions();
-                                        op.SetDestination(destination);
-
-                                        await endpoint.Send(new TestCommand(), op);
-                                    }
-                                    else
-                                    {
-                                        await Task.Delay(TimeSpan.FromSeconds(1), ct);
-                                    }
-                                }
-                                catch
-                                {
-                                    if (ct.IsCancellationRequested)
-                                    {
-                                        return;
-                                    }
-                                }
-                            }
-                        }).ToArray();
-
-                        await Task.WhenAll(new List<Task>(senders) {monitor});
-                    }),
-                ("r|Reset receiver statistics", async (ct, args) => { await endpoint.Send(new ResetCommand()); })
-            };
-
-            await Run(commands);
+            await Task.WhenAll(new List<Task>(senders) {monitor});
         }
+
+        static async Task Send(int numberOfTasks = 5, string destination = null)
+        {
+            var tasks = Enumerable.Range(1, numberOfTasks).Select(async _ =>
+            {
+                while (ct.IsCancellationRequested == false)
+                {
+                    var op = new SendOptions();
+                    if (destination != null)
+                    {
+                        op.SetDestination(destination);
+                    }
+
+                    await endpoint.Send(new TestCommand(), op);
+                }
+            }).ToArray();
+
+            await Task.WhenAll(tasks);
+        }
+
+        static async Task FillQueue(int totalMessages = 1000, int numberOfTasks = 5, string destination = null)
+        {
+            var tasks = Enumerable.Range(1, numberOfTasks).Select(async _ =>
+            {
+                for (var i = 0; i < totalMessages / numberOfTasks; i++)
+                {
+                    var op = new SendOptions();
+                    if (destination != null)
+                    {
+                        op.SetDestination(destination);
+                    }
+
+                    await endpoint.Send(new TestCommand(), op);
+                }
+            }).ToArray();
+
+            await Task.WhenAll(tasks);
+        }
+
 
         static async Task Run((string, Func<CancellationToken, string[], Task>)[] commands)
         {
