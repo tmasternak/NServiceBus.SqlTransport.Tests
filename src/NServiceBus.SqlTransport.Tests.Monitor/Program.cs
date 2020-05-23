e9ef389505a67ca1214a276135d87bddbe806320
Ramon Smits <ramon.smits@gmail.com>
2020-05-23 16:31:05 +0200

    Enhancements and breaking changes:
    ! Must now pass connection string.
    ! AI metric using properties for queue length instead of appending table name
    
    - Prevent multiple instances.
    - Poll interval to 10 seconds.
    - Queue length metric just dumps row count from each table
    - Processing of `wait_stats` track deltas for graphing to make it easier to look at differences over longer times.
    - Network failure will not bring down monitor.

--- src/NServiceBus.SqlTransport.Tests.Monitor/Program.cs
+++ src/NServiceBus.SqlTransport.Tests.Monitor/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
@@ -13,7 +16,19 @@ namespace NServiceBus.SqlTransport.Tests.Monitor
     {
         static async Task Main(string[] args)
         {
-            var endpointName = args.Length > 0 ? args[0] : Shared.Configuration.ReceiverEndpointName;
+            Console.WriteLine(Environment.CommandLine);
+            var id = DeterministicGuid.Create(args[0]);
+            var mutexName = id.ToString("N");
+
+            using var appSingleton = new System.Threading.Mutex(false, mutexName, out var createdNew);
+
+            if (!createdNew)
+            {
+                Console.WriteLine("Single instance!");
+                return;
+            }
+
+            Configuration.ConnectionString = args[0];
 
             var configuration = TelemetryConfiguration.CreateDefault();
             configuration.InstrumentationKey = Configuration.AppInsightKey;
@@ -29,95 +44,148 @@ namespace NServiceBus.SqlTransport.Tests.Monitor
 
             while (true)
             {
-                var queueLengthMetric = await GetQueueLengthMetric(endpointName);
+                try
+                {
+                    await foreach (var item in GetQueueLengthsMetric())
+                    {
+                        telemetryClient.TrackMetric(item);
+                    }
 
-                telemetryClient.TrackMetric(queueLengthMetric);
+                    var pageLatchMetrics = await GetPageLatchStats();
 
-                var pageLatchMetrics = await GetPageLatchStats();
+                    pageLatchMetrics.ToList().ForEach(m => telemetryClient.TrackMetric(m));
 
-                pageLatchMetrics.ToList().ForEach(m => telemetryClient.TrackMetric(m));
+                    telemetryClient.Flush();
 
-                telemetryClient.Flush();
-
-                Console.WriteLine($"[{DateTime.Now.ToLocalTime()}] Metrics pushed to AppInsights");
-
-                await Task.Delay(TimeSpan.FromSeconds(1));
+                    await Task.Delay(TimeSpan.FromSeconds(10));
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(ex);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
             }
         }
 
         static async Task ClearWaitTimeStats()
         {
-            using (var connection = new SqlConnection(Configuration.ConnectionString))
-            {
-                await connection.OpenAsync();
+            using var connection = new SqlConnection(Configuration.ConnectionString);
+            await connection.OpenAsync();
+            using var command = new SqlCommand("DBCC SQLPERF ('sys.dm_os_wait_stats', CLEAR); ", connection);
+            await command.ExecuteNonQueryAsync();
+        }
 
-                using (var command = new SqlCommand("DBCC SQLPERF ('sys.dm_os_wait_stats', CLEAR); ", connection))
+        static async IAsyncEnumerable<MetricTelemetry> GetQueueLengthsMetric()
+        {
+            using var connection = new SqlConnection(Configuration.ConnectionString);
+
+            var query = @"
+
+                SELECT p.rows,t.name
+                FROM sys.partitions AS p
+	                INNER JOIN sys.tables AS t   ON p.[object_id] = t.[object_id]
+	                INNER JOIN sys.schemas AS s  ON s.[schema_id] = t.[schema_id]
+                WHERE
+	                p.rows>0
+	                AND p.index_id IN (0,1);";
+
+            foreach (var table in await connection.QueryAsync(query))
+            {
+                yield return new MetricTelemetry
                 {
-                    await command.ExecuteNonQueryAsync();
-                }
+                    Name = $"Queue Length",
+                    Sum = (int)table.rows,
+                    Count = 1,
+                    Properties = { { "Table", table.name } }
+
+                };
             }
         }
 
-        static async Task<MetricTelemetry> GetQueueLengthMetric(string endpointName)
-        {
-            var queueLength = await QueueLengthMonitor.GetQueueLengthMetric(endpointName);
-           
-            return new MetricTelemetry
-            {
-                Name = $"queue length - {endpointName}",
-                Sum = (int) queueLength,
-                Count = 1
-            };
-        }
+        static wait_stats previous;
 
         static async Task<MetricTelemetry[]> GetPageLatchStats()
         {
-            var query =
-                @"select waiting_tasks_count, wait_time_s, max_wait_time_ms, signal_wait_time_s from qpi.wait_stats where wait_type = 'PAGELATCH_EX'";
+            var query = @"select waiting_tasks_count, wait_time_s, max_wait_time_ms, signal_wait_time_s, wait_per_task_ms from qpi.wait_stats where wait_type = 'PAGELATCH_EX'";
 
-            using (var connection = new SqlConnection(Configuration.ConnectionString))
+            using var connection = new SqlConnection(Configuration.ConnectionString);
+
+            var result = await connection.QueryFirstOrDefaultAsync<wait_stats>(query);
+            result.ticks = Stopwatch.GetTimestamp();
+
+            var delta = result.Subtract(previous);
+
+            Console.WriteLine(delta);
+
+            var metrics = new[]
             {
-                await connection.OpenAsync();
-
-                using (var command = new SqlCommand(query, connection))
+                // From DELTA
+                new MetricTelemetry
                 {
-                    var result = await command.ExecuteReaderAsync();
-                    if (result.HasRows)
-                    {
-                        result.Read();
+                    Name = "PAGELATCH_EX - waiting_tasks_count (delta)",
+                    Sum = delta.waiting_tasks_count,
+                    Count = 1
+                },
+                new MetricTelemetry
+                {
+                    Name = "PAGELATCH_EX - wait_time_s (delta)",
+                    Sum = delta.wait_time_s,
+                    Count = 1
+                },
+                new MetricTelemetry
+                {
+                    Name = "PAGELATCH_EX - signal_wait_time_s (delta)",
+                    Sum = delta.signal_wait_time_s,
+                    Count = 1
+                },
+                
+                // From RESULT
+                new MetricTelemetry
+                {
+                    Name = "PAGELATCH_EX - waiting_tasks_count",
+                    Sum = result.waiting_tasks_count,
+                    Count = 1
+                },
+                new MetricTelemetry
+                {
+                    Name = "PAGELATCH_EX - wait_time_s",
+                    Sum = result.wait_time_s,
+                    Count = 1
+                },
+                new MetricTelemetry()
+                {
+                    Name = "PAGELATCH_EX - max_wait_time_ms",
+                    Sum = result.max_wait_time_ms,
+                    Count = 1
+                },
+                new MetricTelemetry
+                {
+                    Name = "PAGELATCH_EX - signal_wait_time_s",
+                    Sum = result.signal_wait_time_s,
+                    Count = 1
+                },
+                new MetricTelemetry()
+                {
 
-                        return new[]
-                        {
-                            new MetricTelemetry
-                            {
-                                Name = "PAGELATCH_EX - waiting_tasks_count",
-                                Sum = (long?) result[0] ?? 0,
-                                Count = 1
-                            },
-                            new MetricTelemetry
-                            {
-                                Name = "PAGELATCH_EX - wait_time_s",
-                                Sum = decimal.ToDouble((decimal) (result[1] ?? 0)),
-                                Count = 1
-                            },
-                            new MetricTelemetry
-                            {
-                                Name = "PAGELATCH_EX - max_wait_time_ms",
-                                Sum = (long?) result[2] ?? 0,
-                                Count = 1
-                            },
-                            new MetricTelemetry
-                            {
-                                Name = "PAGELATCH_EX - signal_wait_time_s",
-                                Sum = (long?) result[3] ?? 0,
-                                Count = 1
-                            }
-                        };
-                    }
+                    Name = "PAGELATCH_EX - signal_wait_time_s",
+                    Sum = result.signal_wait_time_s,
+                    Count = 1
+                } ,
+                new MetricTelemetry()
+                {
 
-                    return new MetricTelemetry[0];
+                    Name = "PAGELATCH_EX - wait_per_task_ms",
+                    Sum = result.wait_per_task_ms,
+                    Count = 1
                 }
-            }
+            };
+
+            previous = result;
+            return metrics;
+
         }
     }
 }
---
+++ src/NServiceBus.SqlTransport.Tests.Monitor/wait_stats.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+struct wait_stats
+{
+    public double waiting_tasks_count;
+    public double wait_time_s;
+    public double signal_wait_time_s;
+    public long max_wait_time_ms;
+    public long wait_per_task_ms;
+    public long ticks;
+
+    public wait_stats Subtract(wait_stats instance)
+    {
+        float ticksDelta = ticks - instance.ticks;
+        var ticksDeltaFactor = Stopwatch.Frequency / ticksDelta;
+        return new wait_stats
+        {
+            max_wait_time_ms = max_wait_time_ms,
+            wait_per_task_ms = wait_per_task_ms,
+
+            signal_wait_time_s = (signal_wait_time_s - instance.signal_wait_time_s) * ticksDeltaFactor,
+            waiting_tasks_count = (waiting_tasks_count - instance.waiting_tasks_count) * ticksDeltaFactor,
+            wait_time_s = (wait_time_s - instance.wait_time_s) * ticksDeltaFactor,
+
+            ticks = (long)ticksDelta
+        };
+    }
+
+    public override string ToString()
+    {
+        return $"waiting_tasks_count:{waiting_tasks_count,6:N}   wait_time_s:{wait_time_s,7:N3}   max_wait_time_ms:{max_wait_time_ms,6:N}   signal_wait_time_s:{signal_wait_time_s,6:N}   wait_per_task_ms:{wait_per_task_ms,6:N}   ticks:{1000 * ticks / Stopwatch.Frequency,6:N0}ms";
+    }
+
+}
